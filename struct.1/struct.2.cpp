dd11002a869a00ca06072c1d9d24b9adcc320cc0
Juliana Berger <142845649+JulianaStadler@users.noreply.github.com>
2025-05-13 10:43:23 -0300

    Exercícios de struct finalizados

--- struct.1/struct.2.cpp
+++ struct.1/struct.2.cpp
@@ -9,5 +9,174 @@ Implemente funções para:
 	• Atualizar a quantidade de um produto existente.
 	• Listar todos os produtos em estoque, mostrando seus detalhes.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 
+struct Produto{
+    int codigo;
+    char nome[20];
+    int quantidade;
+    float preco;
+};
+
+int menu(){
+	int op;
+	printf( "-------------------------- MENU --------------------------\n"
+			"1 - Adicionar novo produto ao estoque\n"
+			"2 - Visualizar produtos do estoque\n"
+			"3 - Atualizar produto\n"
+			"0 - Sair\n"
+			"----------------------------------------------------------\n"
+			"Digite qual opção você deseja: ");
+	scanf("%d", &op);
+	printf("\n");
+	return op;
+}
+
+int main(){
+	int op, tam = 0;
+	struct Produto* produtos = NULL;
+	
+	do {
+		op = menu();
+		
+		switch(op){
+			case 0:
+				printf("Desligando programa...\n");
+				break;
+			case 1:
+				produtos = (struct Produto*)realloc(produtos, (tam + 1) * sizeof(struct Produto));
+				printf( "----------------- CADASTRAR NOVO PRODUTO -----------------\n"
+		       		    "Digite o código do produto: ");
+			    scanf("%d", &produtos[tam].codigo);
+			    getchar();
+			
+			    printf("Digite o nome do produto: ");
+			    fgets(produtos[tam].nome, sizeof(produtos[tam].nome), stdin);
+			    produtos[tam].nome[strcspn(produtos[tam].nome, "\n")] = 0;
+			
+			    printf("Digite a quantidade: ");
+			    scanf("%d", &produtos[tam].quantidade);
+			    getchar();
+			
+			    printf("Digite o preço: R$");
+			    scanf("%f", &produtos[tam].preco);
+		        tam++;
+		        
+		        printf( "Produto adicionado!\n"
+						"----------------------------------------------------------\n\n");
+				break;
+			case 2:
+				printf( "--------------------- LISTAR PRODUTOS --------------------\n");
+				if(tam <= 0){
+					printf("Nenhum produto cadastrado!\n");
+				}else{
+					for(int i = 0; i < tam; i++){
+						printf(
+							"Código: %d\n"
+							"Nome: %s\n"
+							"Quantidade: %d\n"
+							"Preço: R$%.2f\n"
+							"==========================================================\n",
+							produtos[i].codigo, produtos[i].nome, produtos[i].quantidade, produtos[i].preco
+						);
+					}
+				}
+		        printf( "----------------------------------------------------------\n\n");
+				
+				break;
+			case 3: {
+				printf( "-------------------- ATUALIZAR PRODUTO -------------------\n");
+				
+				if(tam <= 0){
+					printf("Nenhum produto cadastrado!\n"
+		        			"----------------------------------------------------------\n\n");
+					break;
+				}
+				
+				int pesquisa;
+				printf("Digite o código do produto que quer atualizar: ");
+				getchar();
+			    scanf("%d", &pesquisa);
+				
+				
+				printf( "-------------------- ATUALIZAR PRODUTO -------------------\n");
+				bool foi_encontrado = false;
+				int i;
+				
+				for(i = 0; i < tam; i++){
+					
+					if(produtos[i].codigo == pesquisa){
+						foi_encontrado = true;
+						break;
+					}
+					
+				}
+				
+				if(foi_encontrado == false){
+					printf("Este codigo não existe no sistema.\n");
+		        	printf( "----------------------------------------------------------\n\n");
+		        	break;
+				}
+				
+				int atualizar = 0;
+				do{
+					printf(
+						"==========================================================\n"
+						"              Produto encontrado no sistema:\n"
+						"==========================================================\n"
+						"1 | Código: %d\n"
+						"2 | Nome: %s\n"
+						"3 | Quantidade: %d\n"
+						"4 | Preço: R$%.2f\n"
+						"0 | Cancelar\n"
+						"==========================================================\n"
+						"Digite qual opção quer atualizar: ",
+						produtos[i].codigo, produtos[i].nome, produtos[i].quantidade, produtos[i].preco
+					);
+					scanf("%d", &atualizar);
+					getchar();
+					
+					switch (atualizar) {
+			            case 0:
+			                printf("Operação cancelada!\n\n");
+			                break;
+			            case 1:
+			                printf("O código do produto não pode ser atualizado...\n\n");
+			                break;
+			            case 2:
+			                printf("Digite o novo nome do produto: ");
+			                fgets(produtos[i].nome, sizeof(produtos[i].nome), stdin);
+			                produtos[i].nome[strcspn(produtos[i].nome, "\n")] = 0;
+			                break;
+			            case 3:
+			                printf("Digite a quantidade atual de estoque: ");
+			                scanf("%d", &produtos[i].quantidade);
+			                getchar();
+			                break;
+			            case 4:
+			                printf("Digite o novo preço: R$");
+			                scanf("%f", &produtos[i].preco);
+			                getchar();
+			                break;
+			            default:
+			                printf("Opção inválida... Digite novamente...\n\n");
+			                break;
+			        }
+				} while(atualizar != 0);
+				
+		        printf( "----------------------------------------------------------\n\n");
+				break;
+			}
+			default:
+				printf("Opção inválida...\nDigite novamente...\n\n");
+		}
+		
+	} while(op != 0);
+	
+	free(produtos);
+	printf("Programa desligado\n");
+	return 0;
+}
--- struct.1/struct.3.cpp
+++ struct.1/struct.3.cpp
@@ -11,3 +11,150 @@ Implemente funções para:
 	• Encontrar a leitura de temperatura mais alta e a mais baixa.
 	• Calcular a temperatura média de todas as máquinas.
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+struct LeituraTemperatura{
+    char identificador[20];
+    char horario[10];
+    float temperatura;
+};
+
+
+int menu(){
+	int op;
+	printf( "-------------------------- MENU --------------------------\n"
+			"1 - Registrar nova leitura de temperatura\n"
+			"2 - Visualizar todas as leituras\n"
+			"3 - Visualizar a temperatura mais alta e a mais baixa\n"
+			"4 - Calcular a média de temperatura das máquinas\n"
+			"0 - Sair\n"
+			"----------------------------------------------------------\n"
+			"Digite qual opção você deseja: ");
+	scanf("%d", &op);
+	printf("\n");
+	return op;
+}
+
+void listarLeituras(LeituraTemperatura leituras[], int tamanho) {
+    printf( "----------------------------------------------------------\n"
+			"                  Leituras registradas\n"
+			"----------------------------------------------------------\n");
+    if (tamanho == 0) {
+        printf( "Nenhuma leitura de temperatura registrada.\n"
+				"----------------------------------------------------------\n\n");
+        return;
+    }
+
+    for (int i = 0; i < tamanho; i++) {
+        printf("| Máquina:%6s | Horário:%6s | Temperatura:%6.2f°C |\n",
+               leituras[i].identificador, leituras[i].horario, leituras[i].temperatura);
+    }
+    printf("----------------------------------------------------------\n\n");
+}
+
+void maxminLeituras(LeituraTemperatura leituras[], int tamanho) {
+    printf( "----------------------------------------------------------\n"
+			"            Temperatura mais alta e mais baixa\n"
+			"----------------------------------------------------------\n");
+			
+    if (tamanho == 0) {
+        printf( "Nenhuma leitura registrada.\n"
+				"----------------------------------------------------------\n\n");
+        return;
+    }
+
+    int max = 0, min = 0;
+    for (int i = 1; i < tamanho; i++) {
+        if (leituras[i].temperatura > leituras[max].temperatura)
+            max = i;
+        if (leituras[i].temperatura < leituras[min].temperatura)
+            min = i;
+    }
+
+    printf( "Temperatura mais alta: %.2f°C (Máquina: %s, Horário: %s)\n"
+            "Temperatura mais baixa: %.2f°C (Máquina: %s, Horário: %s)\n"
+			"----------------------------------------------------------\n\n",
+			leituras[max].temperatura, leituras[max].identificador, leituras[max].horario,
+            leituras[min].temperatura, leituras[min].identificador, leituras[min].horario);
+}
+
+void calcularMedia(LeituraTemperatura leituras[], int tamanho) {
+    printf( "----------------------------------------------------------\n"
+			"                   Media de temperaturas\n"
+			"----------------------------------------------------------\n");
+			
+    if (tamanho == 0) {
+        printf("Nenhuma leitura registrada.\n");
+	printf("----------------------------------------------------------\n\n");
+        return;
+    }
+
+    float soma = 0;
+    for (int i = 0; i < tamanho; i++) {
+        soma += leituras[i].temperatura;
+    }
+
+    printf("Temperatura média das máquinas: %.2f°C\n", soma / tamanho);
+	printf("----------------------------------------------------------\n\n");
+}
+
+
+int main(){
+	int op, tam = 0;
+	struct LeituraTemperatura leituras[15];
+	
+	do {
+		op = menu();
+		
+		switch(op){
+			case 0:
+				printf("Desligando programa...\n");
+				break;
+			case 1:
+				printf( "----------------- CADASTRAR NOVA LEITURA -----------------\n");
+				
+			    if (tam >= 15) {
+			        printf("Limite de leituras atingido.\n");
+			        break;
+			    }
+			
+			    printf("Digite o identificador da máquina: ");
+			    getchar();
+			    fgets(leituras[tam].identificador, sizeof(leituras[tam].identificador), stdin);
+			    leituras[tam].identificador[strcspn(leituras[tam].identificador, "\n")] = 0;
+			
+			    printf("Digite o horário da leitura: ");
+			    fgets(leituras[tam].horario, sizeof(leituras[tam].horario), stdin);
+			    leituras[tam].horario[strcspn(leituras[tam].horario, "\n")] = 0;
+			
+			    printf("Digite a temperatura registrada: ");
+			    scanf("%f", &leituras[tam].temperatura);
+			
+			    printf("Leitura registrada com sucesso!\n\n");
+		        tam++;
+		        
+		        printf( "Leitura registrada!\n"
+						"----------------------------------------------------------\n\n");
+				break;
+			case 2:
+				listarLeituras(leituras, tam);
+				break;
+			case 3:
+				maxminLeituras(leituras, tam);
+				break;
+			case 4:
+				calcularMedia(leituras, tam);
+				break;
+			default:
+				printf("Opção inválida...\nDigite novamente...\n\n");
+		}
+		
+	} while(op != 0);
+	
+	free(leituras);
+	printf("Programa desligado\n");
+	return 0;
+}
